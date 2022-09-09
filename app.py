d86d423db05721bb304b8b5c0757be9868b54a60
Adith <63020303+Adith2710@users.noreply.github.com>
2022-09-09 07:36:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,38 +55,39 @@ elif choose_type== "URL":
     choose_key = st.radio("Choose Sumarization Type" ,keys)
 
     tokenization_kwargs = {'truncation':True,'max_length':512,'return_tensors':'pt'}
-    if state:
-        url = text
-        html = urlopen(url).read()
-        soup = BeautifulSoup(html, features="html.parser")
+    if text.isalpha()== True:
+        if state:
+            url = text
+            html = urlopen(url).read()
+            soup = BeautifulSoup(html, features="html.parser")
 
-        # kill all script and style elements
-        for script in soup(["script", "style"]):
-            script.extract()    # rip it out
+            # kill all script and style elements
+            for script in soup(["script", "style"]):
+                script.extract()    # rip it out
 
-        # get text
-        text = soup.get_text()
+            # get text
+            text = soup.get_text()
 
-        # break into lines and remove leading and trailing space on each
-        lines = (line.strip() for line in text.splitlines())
-        # break multi-headlines into a line each
-        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-        # drop blank lines
-        text = '\n'.join(chunk for chunk in chunks if chunk)
+            # break into lines and remove leading and trailing space on each
+            lines = (line.strip() for line in text.splitlines())
+            # break multi-headlines into a line each
+            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+            # drop blank lines
+            text = '\n'.join(chunk for chunk in chunks if chunk)
 
-        if choose_key == "Summary Version 2":
-            st.write("Generating Summary Version 2")
-            summarization_long = transformers.pipeline("summarization", model="sshleifer/distilbart-cnn-12-6", tokenizer="sshleifer/distilbart-cnn-12-6")
-            tokenizer_long = transformers.AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-12-6")
-            summary_long_text = tokenizer_long.decode(summarization_long(text, **tokenization_kwargs)[0]["summary_token_ids"])
-            st.write(summary_long_text)
-        elif choose_key == "Summary Version 1":
-            st.write("Generating Summary Version 1")
-            summarization_short = transformers.pipeline("summarization", model="sshleifer/distilbart-cnn-6-6", tokenizer="sshleifer/distilbart-cnn-6-6")
+            if choose_key == "Summary Version 2":
+                st.write("Generating Summary Version 2")
+                summarization_long = transformers.pipeline("summarization", model="sshleifer/distilbart-cnn-12-6", tokenizer="sshleifer/distilbart-cnn-12-6")
+                tokenizer_long = transformers.AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-12-6")
+                summary_long_text = tokenizer_long.decode(summarization_long(text, **tokenization_kwargs)[0]["summary_token_ids"])
+                st.write(summary_long_text)
+            elif choose_key == "Summary Version 1":
+                st.write("Generating Summary Version 1")
+                summarization_short = transformers.pipeline("summarization", model="sshleifer/distilbart-cnn-6-6", tokenizer="sshleifer/distilbart-cnn-6-6")
 
-            tokenizer_short = transformers.AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-6-6")
-            summary_short_text = tokenizer_short.decode(summarization_short(text, **tokenization_kwargs)[0]["summary_token_ids"])
-            st.write(summary_short_text)
+                tokenizer_short = transformers.AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-6-6")
+                summary_short_text = tokenizer_short.decode(summarization_short(text, **tokenization_kwargs)[0]["summary_token_ids"])
+                st.write(summary_short_text)
 
 
 
